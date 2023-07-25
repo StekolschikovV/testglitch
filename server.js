3df58975d9df528d26a965a553d35346ea68df48
Vitaly <v.a.stekolschikov@gmail.com>
2023-07-25 12:12:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,3 +9,29 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+const { Telegraf, Markup } = require("telegraf");
+
+const token = '6645397028:AAGPNe5evoTMKPoWw95a9vq6PP6lWz6roIs'
+
+const bot = new Telegraf(token);
+
+
+bot.command("map", (ctx) => {
+    return ctx.reply(
+        "Карта открыта",
+        Markup.keyboard([
+            Markup.button.webApp(
+                "Карта",
+                "https://asd.web.app/"
+            ),
+        ], {}),
+    );
+});
+// bot.on("message", async (ctx) => {
+//     console.log(ctx.message.web_app_data)
+//     return ctx.reply(ctx.message.web_app_data.data)
+//
+// });
+
+bot.launch();
